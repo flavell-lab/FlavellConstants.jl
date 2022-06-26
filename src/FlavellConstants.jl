cb18f59d0c1a40dfcb53f96a4db7722806afb2ad
Jungsoo Kim <jungsu93@gmail.com>
2022-06-26 14:35:37 -0400

    init commit

---
+++ src/FlavellConstants.jl
@@ -0,0 +1,8 @@
+module FlavellConstants
+include("antsun_data.jl")
+export σ_vel,
+	σ_vT,
+	σ_θh,
+	σ_pumping
+
+end # module
---
+++ src/antsun_data.jl
@@ -0,0 +1,4 @@
+const σ_vel = 0.06030961137253011 # velocity std
+const σ_vT = 0.9425527026496543
+const σ_θh = 0.49429038957075727 # head curvature std
+const σ_pumping = 1.2772001409506841 # pumping std
